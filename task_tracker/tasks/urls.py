8628df29d12e351dd4da9934a0bd64ff282a726d
Dewi Afoko <dewi.afoko@gmail.com>
2025-02-04 15:09:13 +0000

    Task creation handled via form, task deletion button implemented and secure

--- task_tracker/tasks/urls.py
+++ task_tracker/tasks/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import get_tasks, mark_complete
+from .views import get_tasks, mark_complete, mark_incomplete, create_task, delete_task
 
 urlpatterns = [
     path("", get_tasks, name="task-list"),  # URL for the task list
-    path("mark_complete/<int:task_id>/", mark_complete, name="mark-complete" )
+    path("mark_complete/<int:task_id>/", mark_complete, name="mark-complete" ),
+    path("mark_incomplete/<int:task_id>/", mark_incomplete, name="mark-incomplete" ),
+    path("delete_task/<int:task_id>/", delete_task, name="delete-task" ),
+    path("create_task/", create_task, name="create-task"),
 ]
--- task_tracker/tasks/views.py
+++ task_tracker/tasks/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
 from .models import Task
 
 
@@ -11,4 +12,30 @@ def mark_complete(request, task_id):
     task = get_object_or_404(Task, id=task_id)
     task.complete = True
     task.save()
+    return redirect("task-list")
+
+def mark_incomplete(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.complete = False
+    task.save()
+    return redirect("task-list")
+
+def create_task(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        description = request.POST.get("description")
+        date_due = request.POST.get("date_due") or None
+
+        Task.objects.create(name=name, description=description, date_due=date_due)
+
+        return redirect("task-list")
+    
+    return render(request, "tasks/create_task.html")
+
+@require_POST
+def delete_task(request, task_id):
+
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+
     return redirect("task-list")
